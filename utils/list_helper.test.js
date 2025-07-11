9dffad402873f8fe5e03ca16a36cd0f6dc57ca46
Jani Piirainen <piirainen.jani98@gmail.com>
2025-07-11 11:28:17 +0300

    4.5 tehty.

--- utils/list_helper.test.js
+++ utils/list_helper.test.js
@@ -1,6 +1,6 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const listHelper = require('./list_helper')
+const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
   const blogs = []
@@ -11,31 +11,23 @@ test('dummy returns one', () => {
 describe('total likes', () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
       likes: 5,
-      __v: 0
     }
   ]
 
   const listWithMultipleBlogs = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
+      title: 'Blog 1',
+      author: 'Author 1',
+      likes: 3,
     },
     {
-      _id: '5a422aa71b54a676234d17f9',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
+      title: 'Blog 2',
+      author: 'Author 2',
       likes: 7,
-      __v: 0
     }
   ]
 
@@ -46,11 +38,47 @@ describe('total likes', () => {
 
   test('when list has multiple blogs equals the sum of likes', () => {
     const result = listHelper.totalLikes(listWithMultipleBlogs)
-    assert.strictEqual(result, 12)
+    assert.strictEqual(result, 10)
   })
 
   test('when list is empty equals zero', () => {
     const result = listHelper.totalLikes([])
     assert.strictEqual(result, 0)
+  })
+})
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      title: 'Blogi A',
+      author: 'Kirjoittaja A',
+      likes: 7,
+    },
+    {
+      title: 'Blogi B',
+      author: 'Kirjoittaja B',
+      likes: 5,
+    },
+    {
+      title: 'Blogi C',
+      author: 'Kirjoittaja C',
+      likes: 12,
+    },
+    {
+      title: 'Blogi D',
+      author: 'Kirjoittaja D',
+      likes: 12,
+    },
+  ]
+
+  test('returns one of the blogs with most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result.likes, 12)
+    assert.ok(blogs.some(blog => blog.title === result.title && blog.likes === 12))
+  })
+
+  test('returns null for empty list', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
   })
 })

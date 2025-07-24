f7d4b160a9a5cebd25d7d837b784cf3332181384
Jani Piirainen <piirainen.jani98@gmail.com>
2025-07-24 12:36:47 +0300

    Tehtävä 4.15 ja 4.16 tehty.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' })
 
+const usersRouter = require('./routes/users')
 const blogsRouter = require('./controllers/blogs')
+
 const app = express()
 
 const mongoUrl = process.env.TEST_MONGODB_URI || process.env.MONGODB_URI
@@ -10,5 +12,6 @@ mongoose.connect(mongoUrl)
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
---
+++ controllers/users.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
+const getUsers = async (request, response) => {
+  const users = await User.find({}).populate('blogs', { url:1, title:1, author:1 })
+  response.json(users)
+}
+
+const createUser = async (request, response) => {
+  const { username, name, password } = request.body
+
+  // tarkista että kentät ovat olemassa ja tarpeeksi pitkiä
+  if (!username || username.length < 3) {
+    return response.status(400).json({ error: 'username must be at least 3 characters long' })
+  }
+
+  if (!password || password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long' })
+  }
+
+  // tarkista onko käyttäjätunnus jo olemassa
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({ error: 'username must be unique' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+}
+
+module.exports = {
+  getUsers,
+  createUser,
+}

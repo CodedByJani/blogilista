ace14c2cc3e58f9d4def084edede4e8e3372d5ce
Jani Piirainen <piirainen.jani98@gmail.com>
2025-07-24 10:44:24 +0300

    Tehtävä 4.14 tehty.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,83 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const Blog = require('../models/blog')
 
 const blogsRouter = express.Router()
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+// GET all blogs
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+// POST a new blog
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'title and url are required' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
   })
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+// DELETE a blog by ID
+blogsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params
+  console.log('DELETE request received for ID:', id)
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return response.status(400).json({ error: 'malformatted id' })
+    }
+
+    const blog = await Blog.findByIdAndDelete(id)
+
+    if (!blog) {
+      return response.status(404).json({ error: 'blog not found' })
+    }
+
+    response.status(204).end()
+  } catch (error) {
+    console.error('Error deleting blog:', error.message)
+    response.status(500).json({ error: 'internal server error' })
+  }
+})
+
+
+// PUT update blog likes
+blogsRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
+  const { likes } = request.body
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(400).json({ error: 'malformatted id' })
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      { likes },
+      { new: true, runValidators: true, context: 'query' }
+    )
+
+    if (updatedBlog) {
+      response.status(200).json(updatedBlog)
+    } else {
+      response.status(404).json({ error: 'blog not found' })
+    }
+  } catch (error) {
+    console.error('Error updating blog:', error.message)
+    response.status(500).json({ error: 'internal server error' })
+  }
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -41,6 +41,90 @@ test('unique identifier property is named id', async () => {
   expect(blog.id).toBeDefined()
 }, 20000)
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Uusi blogi',
+    author: 'Testaaja',
+    url: 'http://testiblogi.com',
+    likes: 7,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(blog => blog.title)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(titles).toContain('Uusi blogi')
+}, 20000)
+
+test('if likes property is missing, it will default to 0', async () => {
+  const newBlog = {
+    title: 'Blog without likes',
+    author: 'No Likes Author',
+    url: 'http://nolikes.com',
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBe(0)
+})
+
+test('blog without title is not added', async () => {
+  const newBlog = {
+    author: 'No Title Author',
+    url: 'http://notitle.com',
+    likes: 1,
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(400)
+})
+
+test('blog without url is not added', async () => {
+  const newBlog = {
+    title: 'No URL Blog',
+    author: 'No URL Author',
+    likes: 1,
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(400)
+})
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+  const blogToDelete = blogsAtStart.body[0]
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+  expect(blogsAtEnd.body).toHaveLength(initialBlogs.length - 1)
+})
+
+test('a blog can update its likes', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+  const blogToUpdate = blogsAtStart.body[0]
+
+  const updatedData = {
+    likes: blogToUpdate.likes + 10,
+  }
+
+  const response = await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedData)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBe(updatedData.likes)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })

c350963a41222966c3fb885cb3d017b5e179b385
Jani Piirainen <piirainen.jani98@gmail.com>
2025-08-20 13:51:52 +0300

    Virheen korjaus tekstissä

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const blogsRouter = require('./controllers/blogs')
 const loginRouter = require('./controllers/login')
 const tokenExtractor = require('./middleware/tokenExtractor')
 
-// Lisää tämä rivi:
 const testingRouter = require('./controllers/testing')
 
 const app = express()
@@ -21,7 +20,6 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-// Lisää tämä koodi, jotta reitti on käytössä vain testitilassa
 if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
